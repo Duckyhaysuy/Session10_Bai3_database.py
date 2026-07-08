4600eb45de04bac5937e0f5feb67b942be7d6b27
Duckyhaysuy <ducktran123456@gmail.com>
2026-07-08 23:22:48 +0700

    Upload database.py via Git Upload 27NetTeam

---
+++ database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+engine = create_engine(
+    "mysql+pymysql://root:Minhduc@localhost/fastapi",
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
